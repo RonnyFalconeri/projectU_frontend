1255940b81720cbef30f988362c20c50dd968587
Ronny Falconeri <ronny.falconeri@prodyna.com>
2021-08-27 11:42:45 +0200

    more variables, traffic light component created, overview moved to components folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule, Title } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { OverviewComponent } from './overview/overview.component';
+import { OverviewComponent } from './components/overview/overview.component';
+import { TrafficLightComponent } from './components/traffic-light/traffic-light.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    OverviewComponent
+    OverviewComponent,
+    TrafficLightComponent
   ],
   imports: [
     BrowserModule,
