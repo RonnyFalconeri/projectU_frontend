43e9e06d1e534112391e307500e9c129c03d2a7a
Ronny Falconeri <ronny.falconeri@prodyna.com>
2021-08-29 15:18:05 +0200

    project detail info structure

--- src/app/components/project-detail-page/project-detail-page.component.ts
+++ src/app/components/project-detail-page/project-detail-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Project } from 'src/app/shared/models/Project';
+import { Size } from 'src/app/shared/models/Size';
+import { ProjectService } from 'src/app/shared/services/project.service';
 
 @Component({
   selector: 'app-project-detail-page',
@@ -7,7 +11,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectDetailPageComponent implements OnInit {
 
-  constructor() { }
+  project: Project;
+  sizeEnum = Size;
+
+  constructor(private readonly activatedRoute: ActivatedRoute, projectService: ProjectService) {
+    this.activatedRoute.params.subscribe((params) => {
+      this.project = projectService.getProjectById(params.id)
+    });
+  }
 
   ngOnInit(): void {
   }
