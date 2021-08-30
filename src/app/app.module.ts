47103589e9e511ee38ca6c90c4274e2a5475ea65
Ronny Falconeri <ronny.falconeri@prodyna.com>
2021-08-30 10:43:03 +0200

    task bar component files moved to better location

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { OverviewComponent } from './components/overview/overview.component';
 import { TrafficLightComponent } from './components/traffic-light/traffic-light.component';
 import { ProjectDetailPageComponent } from './components/project-detail-page/project-detail-page.component';
-import { TaskBarComponent } from './components/task-bar/task-bar.component';
+import { TaskBarComponent } from './components/project-detail-page/task-bar/task-bar.component';
 
 @NgModule({
   declarations: [
